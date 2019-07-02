c2248bbd2d5715933c3fc40e2d0048608f0963f0
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2019-07-02 20:18:45 -0300

    Implemented method to add the exams to a course

--- portal/course.py
+++ portal/course.py
@@ -1,11 +1,18 @@
+from functools import reduce
+
 class Course:
 
-    def __init__(self, name, id, exams):
+    def __init__(self, name, id, exams = list()):
         self.name = name
         self.id = id
         self.exams = exams
+
+    def to_string(self):
+        return self.name + " [" + self.id + "]\n" + (reduce((lambda x, y: x + '\n' + y), map((lambda x: '\t' + x.to_string()), self.exams)) if len(self.exams) > 0 else "")
 
 class Exam:
     def __init__(self, name, score):
         self.name = name
         self.score = score
+    def to_string(self):
+        return self.name + " " + self.score
